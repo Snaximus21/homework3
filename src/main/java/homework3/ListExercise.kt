83d8e18b1579889a6d9d63ac2f299a95c6c16269
Aleksandr <email>
2023-05-10 11:23:05 +0300

    Комментарий

--- src/main/java/homework3/ListExercise.kt
+++ src/main/java/homework3/ListExercise.kt
@@ -1,9 +1,9 @@
 package homework3
 
 fun main(){
-    var numberList = MutableList(100) {it}
+    var numberList = MutableList(100) {it}//Создайте список и заполните его целыми числами 0-99
     println(numberList)
-    numberList = numberList.filter { (it % 2 == 0) }.toMutableList()
+    numberList = numberList.filter { (it % 2 == 0) }.toMutableList()//Удаляет из цепочки все нечетные числа
     println(numberList)
     val slice = numberList.slice(10..20).toMutableList()// Берет слайс с 10 по 20 элементы
     slice.replaceAll { (it + 1) }//и добавляет к каждому элементу 1
